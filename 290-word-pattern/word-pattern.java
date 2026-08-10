d8e90b387ff334f874669a2da2ea4c74ef1385a0
Challa Satyanarayana <challasatyanarayana14@gmail.com>
2026-08-10 12:44:07 +0530

    Time: 1 ms (83.61%) | Memory: 42.7 MB (56.08%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,29 +1,47 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        int n=pattern.length();
+
+        char[] pat=pattern.toCharArray();
         String[] words=s.split(" ");
-        if(words.length!=n) return false;
-        HashMap<Character,String> hs1=new HashMap<>();
-        HashMap<String,Character> hs2=new HashMap<>();
-        
-        for(int i=0;i<n;i++){
-            char ch1=pattern.charAt(i);
-            String ch2=words[i];
-            if(hs1.containsKey(ch1)){
-                if(!hs1.get(ch1).equals(ch2)){
-                    return false;
-                }
-            }else{
-                hs1.put(ch1,ch2);
+        int pl=pat.length;
+        int wl=words.length;
+        if(pl!=wl) return false;
+
+        HashMap<Character,String> hs=new HashMap<>();
+        for(int i=0;i<pl;i++){
+            char ch=pat[i];
+            String wo=words[i];
+            if(hs.containsKey(ch)){
+            if(!hs.get(ch).equals(wo)) return false;
             }
-            if(hs2.containsKey(ch2)){
-                if(hs2.get(ch2)!=ch1){
-                    return false;
-                }
-            }else{
-                hs2.put(ch2,ch1);
-            }
+            else if(hs.containsValue(wo)) return false;
+            hs.put(ch,wo);
         }
         return true;
+        // int n=pattern.length();
+        // String[] words=s.split(" ");
+        // if(words.length!=n) return false;
+        // HashMap<Character,String> hs1=new HashMap<>();
+        // HashMap<String,Character> hs2=new HashMap<>();
+        
+        // for(int i=0;i<n;i++){
+        //     char ch1=pattern.charAt(i);
+        //     String ch2=words[i];
+        //     if(hs1.containsKey(ch1)){
+        //         if(!hs1.get(ch1).equals(ch2)){
+        //             return false;
+        //         }
+        //     }else{
+        //         hs1.put(ch1,ch2);
+        //     }
+        //     if(hs2.containsKey(ch2)){
+        //         if(hs2.get(ch2)!=ch1){
+        //             return false;
+        //         }
+        //     }else{
+        //         hs2.put(ch2,ch1);
+        //     }
+        // }
+        // return true;
     }
 }
