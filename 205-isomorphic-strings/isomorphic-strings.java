a541216dcf0ee366b73b8452e00731997043d40d
Challa Satyanarayana <challasatyanarayana14@gmail.com>
2026-08-10 11:40:38 +0530

    Time: 5 ms (98.62%) | Memory: 43.8 MB (59.19%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -3,28 +3,42 @@ class Solution {
         int m=s.length();
         int n=t.length();
         if(m!=n) return false;
-        HashMap<Character,Character> hs1=new HashMap<>();
-        HashMap<Character,Character> hs2=new HashMap<>();
+
+        int[] s1=new int[256];
+        int[] t1=new int[256];
         for(int i=0;i<n;i++){
             char ch1=s.charAt(i);
             char ch2=t.charAt(i);
-            if(hs1.containsKey(ch1)){
-                if(hs1.get(ch1)!=ch2){
-                    return false;
-                }
-            }else{
-                hs1.put(ch1,ch2);
+            if(s1[ch1]!=t1[ch2]){
+                return false;
             }
-            if(hs2.containsKey(ch2)){
-                if(hs2.get(ch2)!=ch1){
-                    return false;
-                }
-            }else{
-                hs2.put(ch2,ch1);
-            }
+            s1[ch1]=i+1;
+            t1[ch2]=i+1;
+
+
+        }
+        // HashMap<Character,Character> hs1=new HashMap<>();
+        // HashMap<Character,Character> hs2=new HashMap<>();
+        // for(int i=0;i<n;i++){
+        //     char ch1=s.charAt(i);
+        //     char ch2=t.charAt(i);
+        //     if(hs1.containsKey(ch1)){
+        //         if(hs1.get(ch1)!=ch2){
+        //             return false;
+        //         }
+        //     }else{
+        //         hs1.put(ch1,ch2);
+        //     }
+        //     if(hs2.containsKey(ch2)){
+        //         if(hs2.get(ch2)!=ch1){
+        //             return false;
+        //         }
+        //     }else{
+        //         hs2.put(ch2,ch1);
+        //     }
             // hs1.put(ch1,hs1.getOrDefault(ch1,0)+1);
             // hs2.put(ch2,hs2.getOrDefault(ch2,0)+1);
-        }
+        //}
         // List<Map.Entry<Character,Integer>> list1=new ArrayList<>(hs1.entrySet());
         // List<Map.Entry<Character,Integer>> list2=new ArrayList<>(hs2.entrySet());
 
