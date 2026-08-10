64c6ce3ac0ec6fd7aa44f8ab955b55deafd06c87
Challa Satyanarayana <challasatyanarayana14@gmail.com>
2026-08-10 14:02:31 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (99.69%) - LeetSync

--- 926-find-and-replace-pattern/find-and-replace-pattern.java
+++ 926-find-and-replace-pattern/find-and-replace-pattern.java
@@ -1,37 +1,49 @@
 class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
         List<String> ans=new ArrayList<>();
-        int n=words.length;
-        int j=0;
-        char[] p=pattern.toCharArray();
-        while(j<n){
-            char[] s1=words[j++].toCharArray();
-            int pl=p.length;
-            int sl=s1.length;
-            if(pl!=sl) break;
-            boolean found=true;
-            HashMap<Character,Character> hs=new HashMap<>();
-            for(int i=0;i<pl;i++){
-                char v1=p[i];
-                char v2=s1[i];
-                if(hs.containsKey(v1)){
-                    if((hs.get(v1)!=v2)){
-                        found=false;
-                        break;
-                    }
-                    }else{
-                         if(hs.containsValue(v2)){
-                        found=false;
-                        break;
-                    }
-                    hs.put(v1,v2);
-                }
-            }
-            if(found){
-                ans.add(new String(s1));
-            }
-
+        for(String str:words){
+            if(check(str,pattern)) ans.add(str);
         }
         return ans;
+        // List<String> ans=new ArrayList<>();
+        // int n=words.length;
+        // int j=0;
+        // char[] p=pattern.toCharArray();
+        // while(j<n){
+        //     char[] s1=words[j++].toCharArray();
+        //     int pl=p.length;
+        //     int sl=s1.length;
+        //     if(pl!=sl) break;
+        //     boolean found=true;
+        //     HashMap<Character,Character> hs=new HashMap<>();
+        //     for(int i=0;i<pl;i++){
+        //         char v1=p[i];
+        //         char v2=s1[i];
+        //         if(hs.containsKey(v1)){
+        //             if((hs.get(v1)!=v2)){
+        //                 found=false;
+        //                 break;
+        //             }
+        //             }else{
+        //                  if(hs.containsValue(v2)){
+        //                 found=false;
+        //                 break;
+        //             }
+        //             hs.put(v1,v2);
+        //         }
+        //     }
+        //     if(found){
+        //         ans.add(new String(s1));
+        //     }
+
+        // }
+        // return ans;
+    }
+    private static boolean check(String str,String pattern){
+        int n=str.length();
+        for(int i=0;i<n;i++){
+            if(str.indexOf(str.charAt(i))!=pattern.indexOf(pattern.charAt(i))) return false;
+        }
+        return true;
     }
 }
