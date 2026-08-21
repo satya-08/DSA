6e1f0c9f0328b67d9972388a5b9990c070c750ec
Challa Satyanarayana <challasatyanarayana14@gmail.com>
2026-08-21 16:36:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (62.91%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
@@ -5,14 +5,14 @@ class Solution {
         int maxcount=0;
         for(int i=1;i<n;i++){
             if(colors[0]!=colors[i]){
-                maxcount=Math.max(maxcount,i);
+                maxcount=Math.max(maxcount,Math.max(i,n-i-1));
             }
         }
-        for(int i=n-2;i>=0;i--){
-            if(colors[n-1]!=colors[i]){
-                maxcount=Math.max(maxcount,n-i-1);
-            }
-        }
+        // for(int i=n-2;i>=0;i--){
+        //     if(colors[n-1]!=colors[i]){
+        //         maxcount=Math.max(maxcount,n-i-1);
+        //     }
+        // }
         return maxcount;
     }
 }
