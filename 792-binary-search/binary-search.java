279e2b9bb0e3badeb820e5180cdc6216b60c32a4
Challa Satyanarayana <challasatyanarayana14@gmail.com>
2026-08-28 12:11:43 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (85.37%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int l=0;
+        int r=arr.length-1;
+        while(l<=r) {
+			int mid=l+(r-l)/2;
+			if(arr[mid]==target) return mid;
+			else if(arr[mid]>target) {
+				r=mid-1;
+			}else l=mid+1;
+		}
+		return -1;
+    }
+}
