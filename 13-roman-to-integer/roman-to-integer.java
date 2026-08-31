cad2e96ed63a2c9d9bda5a365c5ab94e2cd6e3d3
Challa Satyanarayana <challasatyanarayana14@gmail.com>
2026-08-31 20:43:31 +0530

    Time: 2 ms (100.00%) | Memory: 46.8 MB (27.89%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,27 +1,79 @@
-import java.util.HashMap;
-
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
+        int sum=0;
+        switch (s.charAt(0)) {
+            case 'I':
+                sum+=1;
+                break;
 
-        int total = 0;
-        int prev = 0;
-
-        for (int i = s.length() - 1; i >= 0; i--) {
-            int curr = map.get(s.charAt(i));
-            if (curr < prev)
-                total -= curr;
-            else
-                total += curr;
-            prev = curr;
+             case 'X':
+                sum+=10;
+                break;
+                    
+            case 'V':
+                sum+=5;
+                break;
+            case 'L':
+                sum+=50;
+                break;
+            case 'C':
+                sum+=100;
+                break;
+            case 'D':
+                sum+=500;
+                break;
+            case 'M':
+                sum+=1000;
+                break;
         }
-        return total;
+        for(int i=1;i<s.length();i++){
+            
+            if(s.charAt(i)=='I'){
+                sum+=1;
+            }
+            else if(s.charAt(i)=='V' ){
+                if(s.charAt(i-1)=='I'){
+                    sum+=3;
+                }else{
+                    sum+=5;
+                }
+            }
+            else if(s.charAt(i)=='X'){
+                if(s.charAt(i-1)=='I'){
+                    sum+=8;
+                }else{
+                    sum+=10;
+                }
+            }
+            else if(s.charAt(i)=='L'){
+                if(s.charAt(i-1)=='X'){
+                    sum+=30;
+                }else{
+                    sum+=50;
+                }
+            }
+            else if(s.charAt(i)=='C'){
+                if(s.charAt(i-1)=='X'){
+                    sum+=80;
+                }else{
+                    sum+=100;
+                }
+            }
+            else if(s.charAt(i)=='D'){
+                if(s.charAt(i-1)=='C'){
+                    sum+=300;
+                }else{
+                    sum+=500;
+                }
+            }
+            else if(s.charAt(i)=='M'){
+                if(s.charAt(i-1)=='C'){
+                    sum+=800;
+                }else{
+                    sum+=1000;
+                }
+            }
+        }
+        return sum;
     }
 }
